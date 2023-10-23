fb4c5bec753132d13b085f86fd7b086d0829ecba
GJ Tiquia <gershom.tiquia@gmail.com>
2023-10-23 12:25:50 +0800

    chore: setup basic html

---
+++ apps/web/index.js
@@ -0,0 +1,17 @@
+function initialize() {
+    // TODO : Add onClick events to buttons
+}
+
+function increment() {
+    console.log("increment!");
+    // TODO : Link to Counter
+}
+
+function decrement() {
+    console.log("decrement!");
+    // TODO : Link to Counter
+}
+
+console.log("Initializing...");
+initialize();
+console.log("Initialized!");
